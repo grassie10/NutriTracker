383dbb9f6afdc78c77ac15037f88fbec3908544f
Jacob <jacobkwat@gmail.com>
2021-10-25 22:31:18 -0500

    Finished filtering list of foods

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import React, { useState } from "react";
-import "./App.css";
-import SearchBar from "./components/SearchBar";
-import { useData } from "./firebase";
+import React, { useState } from 'react';
+import './App.css';
+import SearchBar from './components/SearchBar';
+import { useData } from './firebase';
 
 function App() {
-  const [data, loading, error] = useData("/");
-  const [searchFilter, setSearchFilter] = useState("");
+  const [data, loading, error] = useData('/');
+  const [foods, setFoods] = useState([]);
 
   if (loading) return <h1>Loading...</h1>;
   if (error) return <h1>Error :(</h1>;
 
-  const filteredFoods = data.filter((item) =>
-    item.name.toLowerCase().startsWith(searchFilter.toLowerCase())
-  );
-  return (
-    <div className="App">
-      <SearchBar setSearchFilter={setSearchFilter} />
+  console.log(foods);
 
-      <ul>
-        {filteredFoods.map((item) => (
-          <li key={item}>{item.name}</li>
-        ))}
-      </ul>
+  return (
+    <div className='App'>
+      <h1>NutriTracker</h1>
+      <SearchBar data={data} foods={foods} setFoods={setFoods} />
     </div>
   );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,21 +1,68 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Dropdown, FormControl } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const SearchBar = ({ setSearchFilter }) => {
-  const handleChange = (event) => {
-    setSearchFilter(event.target.value);
-  };
+const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+  <button
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  >
+    {children}
+    &#x25bc;
+  </button>
+));
+
+// forwardRef again here!
+// Dropdown needs access to the DOM of the Menu to measure it
+const CustomMenu = React.forwardRef(
+  ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
+    const [value, setValue] = useState('');
+
+    return (
+      <div
+        ref={ref}
+        style={style}
+        className={className}
+        aria-labelledby={labeledBy}
+      >
+        <FormControl
+          autoFocus
+          className='mx-3 my-2 w-auto'
+          placeholder='Type to filter...'
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+        />
+        <ul className='list-unstyled'>
+          {React.Children.toArray(children).filter(
+            (child) =>
+              !value ||
+              child.props.children.toLowerCase().startsWith(value.toLowerCase())
+          )}
+        </ul>
+      </div>
+    );
+  }
+);
+
+const SearchBar = ({ data, foods, setFoods }) => {
+
 
   return (
-    <form action="/" method="get">
-      <input
-        onChange={handleChange}
-        type="text"
-        id="header-search"
-        placeholder="Search Food Items"
-        name="s"
-      />
-      <button type="submit">Search</button>
-    </form>
+    <Dropdown>
+      <Dropdown.Toggle as={CustomToggle} id='dropdown-custom-components'>
+        Select Food Items
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu as={CustomMenu}>
+        {data.map((item, index) => (
+          <Dropdown.Item key={index} onClick={() => setFoods([...foods, item])}>
+            {item.name}
+          </Dropdown.Item>
+        ))}
+      </Dropdown.Menu>
+    </Dropdown>
   );
 };
 
