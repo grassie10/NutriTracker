c3d6b8227fa638c0f91ca053b4bf0613250cdf7c
Carolyn Perniciaro <cmpvm5@mail.missouri.edu>
2021-10-27 22:31:59 -0500

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,51 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { screen } from "@testing-library/react";
+import App from "./App";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import NutrientCount from "./components/NutrientCount";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders with or without a name", () => {
+  act(() => {
+    render(<App />, container);
+  });
+  expect(container.textContent).toBe("Loading...");
+
+  // act(() => {
+  //   render(<NutrientCount />, container);
+  // });
+  // expect(container.textContent).toBe("Average Calories: 2250 cal");
+
+  // act(() => {
+  //   render(<App />, container);
+  // });
+  // expect(container.textContent).toContainElement("Nutritracker");
+  // act(() => {
+  //   render(<Hello name="Jenny" />, container);
+  // });
+  // expect(container.textContent).toBe("Hello, Jenny!");
+
+  // act(() => {
+  //   render(<Hello name="Margaret" />, container);
+  // });
+  // expect(container.textContent).toBe("Hello, Margaret!");
 });
