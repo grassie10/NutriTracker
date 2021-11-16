5ec0803261cd4bda0a88b60b967fd016257fc1ee
Carolyn Perniciaro <cmpvm5@mail.missouri.edu>
2021-11-15 19:21:04 -0600

    updated test

--- src/tests/InputDetails.test.js
+++ src/tests/InputDetails.test.js
@@ -1,38 +1,76 @@
-import { render, screen, act, fireEvent } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import App from '../App';
-import InputDetails from '../components/InputDetails';
+import { render, screen, act, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "../App";
+import InputDetails from "../components/InputDetails";
 
-describe('age arrow changes input', () => {
+describe("age arrow changes input", () => {
   var foods = [];
   const setFoods = (newList) => (foods = newList);
 
-  test('button click', async () => {
-    const { getByTestId } = render(
-      <InputDetails />
-    );
-    const ageInput = getByTestId('age-input');
-    fireEvent.change(ageInput, {target: {value: 20}});
+  test("button click", async () => {
+    const { getByTestId } = render(<InputDetails />);
+    const ageInput = getByTestId("age-input");
+    fireEvent.change(ageInput, { target: { value: 20 } });
 
-    expect(ageInput.value).toContain('20');
+    expect(ageInput.value).toContain("20");
   });
 
-  test('Checking height input', async () => {
-    const { getByTestId } = render(
-      <InputDetails />
-    );
-    const heightInput = getByTestId('height-input');
-    fireEvent.change(heightInput, {target: {value: 66}});
-    expect(heightInput.value).toContain('66');
+  test("Checking height input", async () => {
+    const { getByTestId } = render(<InputDetails />);
+    const heightInput = getByTestId("height-input");
+    fireEvent.change(heightInput, { target: { value: 66 } });
+    expect(heightInput.value).toContain("66");
   });
 
-  test('Checking weight input', async () => {
-    const { getByTestId } = render(
-      <InputDetails />
-    );
-    const weightInput = getByTestId('weight-input');
-    fireEvent.change(weightInput, {target: {value: 75}});
-    expect(weightInput.value).toContain('75');
+  test("Checking weight input", async () => {
+    const { getByTestId } = render(<InputDetails />);
+    const weightInput = getByTestId("weight-input");
+    fireEvent.change(weightInput, { target: { value: 75 } });
+    expect(weightInput.value).toContain("75");
   });
+});
 
+describe("activity level selection changes input", () => {
+  test("select values", () => {
+    const { getByTestId } = render(
+      <select data-testid="activity-select">
+        <option data-testid="val0" value="0" title="Little or no exercise">
+          Sedentary
+        </option>
+        <option
+          value="1"
+          data-testid="val1"
+          title="Light exercise/sports 1-3 days/week"
+        >
+          Lightly active
+        </option>
+        <option
+          value="2"
+          data-testid="val2"
+          title="Moderate exercise/sports 3-5 days/week"
+        >
+          Moderately active
+        </option>
+        <option
+          value="3"
+          data-testid="val3"
+          title="Hard exercise/sports 6-7 days a week"
+        >
+          Very active
+        </option>
+        <option
+          data-testid="val4"
+          value="4"
+          title="Very hard exercise/sports, physical job or 2x training"
+        >
+          Extra active
+        </option>
+      </select>
+    );
+
+    userEvent.selectOptions(getByTestId("activity-select"), ["1"]);
+
+    expect(getByTestId("val1").selected).toBe(true);
+    expect(getByTestId("val2").selected).toBe(false);
+  });
 });
